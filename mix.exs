0faa116069b8cfc5f6394cd1927aa6925d3eea18
Garth Kidd <garth@garthk.com>
2020-05-16 17:06:18 +1000

    Publish 1.0.5.

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule Pretty.MixProject do
       source_url: "https://github.com/amplifiedai/pretty",
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
-      version: "1.0.4"
+      version: "1.0.5"
     ]
   end
 
